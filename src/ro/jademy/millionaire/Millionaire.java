8db3c11b3ee40a5f738f3f19f9749b7c39b3d275
Remus Irimia <rms.irimia@gmail.com>
2018-09-20 18:25:54 +0300

    Millionaire_V1.1

--- src/ro/jademy/millionaire/Millionaire.java
+++ src/ro/jademy/millionaire/Millionaire.java
@@ -1,18 +1,33 @@
 package ro.jademy.millionaire;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Millionaire {
 
+
+
     public static void main(String[] args) {
 
         Game game = new Game();
+
         showStartMessage();
 
-        game.askQuestion();
+        ArrayList<Question> questions = new ArrayList<>();
+        List<Answer> answers = new ArrayList<>();
+
+        Answer answer1Question1 = new Answer("Fotball", false);
+        Answer answer2Question1 = new Answer("Who wants to be a millionaire", true);
+        Answer answer3Question1 = new Answer("Winning Lines", false);
+        Answer answer4Question1 = new Answer("Duel", false);
+
+        answers.add(answer1Question1);
+        answers.add(answer2Question1);
+        answers.add(answer3Question1);
+        answers.add(answer4Question1);
+
+        Question question1 = new Question("Cum se numeste jocul?", 1,answers);
+        questions.add(question1);
 
 
     }
